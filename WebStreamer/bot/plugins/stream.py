55b1aa6beb721af5f13c4d59bdb8c89406be6a46
hemanthbot69 <104711832+hemanthbot69@users.noreply.github.com>
2022-05-06 20:03:41 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -28,7 +28,7 @@ async def media_receive_handler(_, m: Message):
     stream_link = Var.URL + str(log_msg.message_id) + '/' +quote_plus(file_name) if file_name else ''
     stream_links = f"https://KalviDudes.in"
     await m.reply_text(
-        text=f"https://droplink.kalvidudes.in/st?api=091f2c0a78b3766954c7dae3dffed779d8740a30&url={stream_link}",
+        text=f"=https://shorturllink.in/st?api=47a176bb17a85c85d2dd137538c04cb06af3286f&url={stream_link}",
         quote=True,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Learn something interesting 🤔', url=stream_links)]])
     )
